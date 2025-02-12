c0c19a2073e7a5761b40d74eb99f3825b0b7f3cf
Abrar Mahir Esam <87098848+Abrar118@users.noreply.github.com>
2025-02-12 13:12:14 +0600

    bug/fix-theme-issue-abrar #1

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -14,7 +14,7 @@ export default function ErrorPage() {
         </p>
         <div className="mt-6">
           <Link
-            to="/home"
+            to="/"
             className="inline-flex items-center rounded-md bg-primary px-4 py-2 text-sm font-medium 
             text-primary-foreground shadow-sm transition-colors hover:bg-primary/90 focus:outline-none 
             focus:ring-2 focus:ring-primary focus:ring-offset-2"
